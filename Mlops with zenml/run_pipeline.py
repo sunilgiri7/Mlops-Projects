568cab61f0fac0a7c4f710a4b8cfff7711cbf5f9
sunilgiri7 <seungiri841@gmail.com>
2024-01-03 11:09:31 -0800

    first commit

---
+++ Mlops with zenml/run_pipeline.py
@@ -0,0 +1,4 @@
+from pipelines.training_pipeline import training_pipeline
+
+if __name__ == '__main__':
+    training_pipeline(data_path="C:\OneDrive\Desktop\Mlops with zenml\data\olist_customers_dataset.csv")
